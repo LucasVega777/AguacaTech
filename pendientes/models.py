2f5ecedb696ef1204d41e903e352d6350120ce66
Jessica Amarilla <jessandrea@Jessicas-MacBook-Pro.local>
2019-04-29 14:54:52 -0400

    se agrego la clase de becas

--- pendientes/models.py
+++ pendientes/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
+from django.db import models
+
 # Create your models here.
 
-class Tarea(models.Model):
-    titulo = models.CharField(max_length=100) #Campo/columna titulo de tipo "campo de caracteres" de longitud maxima de 100
+class Becas(models.Model): #solo clases en mayuscula
+    programa = models.CharField(max_length=100) #Campo/columna titulo de tipo "campo de caracteres" de longitud maxima de 100
     descripcion = models.TextField(null=True, blank=True) #Campo/columna titulo de tipo Texto, los argumentos blank y null son para que el campo sea opcional
-    estado = models.BooleanField(default=False)
-
+    requisito = models.TextField(null=True, blank=True)
+    duracion = models.CharField(max_length=50)
+    contactos = models.TextField(null=True, blank=True)
+    
     def __str__(self):
         return self.titulo
-
