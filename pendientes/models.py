bd8fe111ff0b49f399c5685c1cf1fb59153af502
Jessica Amarilla <jessandrea@Jessicas-MacBook-Pro.local>
2019-04-29 15:49:44 -0400

    se agregaron choices

--- pendientes/models.py
+++ pendientes/models.py
@@ -3,9 +3,15 @@ from django.db import models
 from django.db import models
 
 # Create your models here.
+tipo_de_beca = (
+        ('g', 'grado'),
+        ('p', 'postgrado'),
+        ('d', 'doctorado'),
+        )
 
 class Becas(models.Model): #solo clases en mayuscula
-    programa = models.CharField(max_length=100) #Campo/columna titulo de tipo "campo de caracteres" de longitud maxima de 100
+    tipo = models.CharField(max_length=1, choices=tipo_de_beca)
+    nombre_programa = models.CharField(max_length=100) #Campo/columna titulo de tipo "campo de caracteres" de longitud maxima de 100
     descripcion = models.TextField(null=True, blank=True) #Campo/columna titulo de tipo Texto, los argumentos blank y null son para que el campo sea opcional
     requisito = models.TextField(null=True, blank=True)
     duracion = models.CharField(max_length=50)
