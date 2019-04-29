d28ef139e23c304aa30bf7cd4ae4518b89cf0154
LucasVega777 <lucasmvegap@gmail.com>
2019-04-29 18:24:27 -0400

    se vacio la carpeta

--- pendientes/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Tarea #importamos el modelo
-
-# Register your models here.
-
-admin.site.register(Tarea) # lo registramos
--- pendientes/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PendientesConfig(AppConfig):
-    name = 'pendientes'
