ae3ed44812ebdbfb338eac243b8897c1efdecdf0
Sandra Silvano <sandrasilvano@Sandras-MacBook-Air.local>
2019-04-30 13:24:24 -0400

    se modifico la base de datos

--- becas/admin.py
+++ becas/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
-from .models import Becas #importamos el modelo
+from .models import Becas 
+
+from .models import TipoBeca
+
+#importamos el modelo
 
 # Register your models here.
 
 admin.site.register(Becas) # lo registramos
+
+admin.site.register(TipoBeca)
